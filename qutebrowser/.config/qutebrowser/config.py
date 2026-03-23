3c90768e0e08c718e12d6f8939375f052abc15e1
Alex Chernika <me@chernika.dev>
2026-03-23 10:11:27 +0100

    qutebrowser: use whitelist for JS on websites

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import os
+
 config.load_autoconfig()
 
 c.window.title_format = "qutebrowser"
@@ -8,11 +10,20 @@ c.editor.command = ["emacsclient", "-c", "-a ''", "{file}"]
 
 c.url.default_page = "~/.config/qutebrowser/startpage.html"
 c.url.start_pages = ["~/.config/qutebrowser/startpage.html"]
-c.url.searchengines = {
-    "DEFAULT":  "https://google.com/search?hl=en&q={}",
-}
+c.url.searchengines = { "DEFAULT":  "https://www.duckduckgo.com/lite/?q={}" }
 
 c.content.blocking.method = "both"
+c.content.javascript.enabled = False
 
-config.source("qutemacs.py") # keybinds
-config.source("gruvbox.py") # colorscheme
+# Add something like https://*.youtube.com/* to js_whitelist.txt to enable
+# JavaScript on that website. Won't recommend it though.
+js_whitelist_path = os.path.join(config.configdir, "js_whitelist.txt")
+if os.path.exists(js_whitelist_path):
+    with open(js_whitelist_path, 'r') as f:
+        for line in f:
+            pattern = line.strip()
+            if pattern and not pattern.startswith('#'):
+                config.set("content.javascript.enabled", True, pattern)
+
+config.source("qutemacs.py")
+config.source("gruvbox.py")
