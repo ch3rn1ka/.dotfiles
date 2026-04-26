02145c32f4f6406517c1227b9bed88cb65fa6e1e
Alex Chernika <me@chernika.dev>
2026-04-26 15:39:04 +0200

    qutebrowser: stop whitelisting websites for js
    As much as I want to limit fingerprinting on the Web, it's very
    tiresome to manually allow JavaScript on any new website I visit.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -15,18 +15,7 @@ c.url.searchengines = { 'DEFAULT': 'https://www.duckduckgo.com/lite/?q={}' }
 c.content.blocking.method = 'both'
 c.content.headers.referer = 'same-domain'
 c.content.cookies.accept = 'no-3rdparty'
-c.content.javascript.enabled = False
-
-# Add something like https://*.youtube.com/* to whitelist.txt to enable
-# JavaScript and save cookies on that website. Won't recommend it though.
-whitelist_path = os.path.join(config.configdir, 'whitelist.txt')
-if os.path.exists(whitelist_path):
-    with open(whitelist_path, 'r') as f:
-        for line in f:
-            pattern = line.strip()
-            if pattern and not pattern.startswith('#'):
-                config.set('content.cookies.accept', 'all', pattern)
-                config.set('content.javascript.enabled', True, pattern)
+c.content.javascript.enabled = True
 
 config.source('qutemacs.py')
 config.source('gruvbox.py')
