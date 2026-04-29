da3b1d6d10a9cd071fa1f35c1cc610a1857cf16c
Alex Chernika <me@chernika.dev>
2026-04-29 13:54:01 +0200

    emacs: install and configure mu4e

--- emacs/.config/emacs/elisp/packages.el
+++ emacs/.config/emacs/elisp/packages.el
@@ -189,4 +189,24 @@
 (use-package which-key
   :init (which-key-mode 1))
 
+(use-package mu4e
+  :config
+  (setq mu4e-change-filenames-when-moving t)
+
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq mu4e-maildir "~/mail")
+
+  (setq mu4e-drafts-folder "/[Gmail]/Drafts")
+  (setq mu4e-sent-folder "/[Gmail]/Sent Mail")
+  (setq mu4e-refile-folder "/[Gmail]/All Mail")
+  (setq mu4e-trash-folder "/[Gmail]/Trash")
+
+  (setq mu4e-maildir-shortcuts
+        '(("/Inbox"             . ?i)
+          ("/[Gmail]/Sent Mail" . ?s)
+          ("/[Gmail]/Trash"     . ?t)
+          ("/[Gmail]/Drafts"    . ?d)
+          ("/[Gmail]/All Mail"  . ?a))))
+
 (provide 'packages)
