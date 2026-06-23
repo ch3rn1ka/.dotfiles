b0c98d26c9e4f7e9b35b03b3dfff25430c5e096d
Alex Chernika <me@chernika.dev>
2026-06-23 22:52:00 +0200

    qutebrowser: bring back cookie whitelist

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,4 +1,5 @@
 # A simple qutebrowser configuration by Alex Chernika <me@chernika.dev>
+import os
 
 config.load_autoconfig()
 
@@ -14,8 +15,17 @@ c.url.searchengines = {'DEFAULT': 'https://www.duckduckgo.com/lite/?q={}'}
 
 c.content.blocking.method = 'both'
 c.content.headers.referer = 'same-domain'
-c.content.cookies.accept = 'no-3rdparty'
-c.content.javascript.enabled = True
+c.content.cookies.accept = 'never'
 
-config.source('qutemacs.py') # keybinds
-config.source('gruvbox.py')  # colorscheme
+# Put a domain in ./whitelist.txt to enable cookies on that website.
+# Example: *://*.google.com/*
+whitelist_path = os.path.join(config.configdir, 'whitelist.txt')
+if os.path.exists(whitelist_path):
+    with open(whitelist_path, 'r') as f:
+        for line in f:
+            pattern = line.strip()
+            if pattern and not pattern.startswith('#'):
+                config.set('content.cookies.accept', 'all', pattern)
+
+config.source('qutemacs.py') # Keybinds
+config.source('gruvbox.py') # Colorscheme
