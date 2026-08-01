c819f6960e8e04d488c84637059303c5869695e2
Alex Chernika <me@chernika.dev>
2026-08-01 14:52:09 +0200

    qutebrowser: temporary accept all cookies

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -15,17 +15,17 @@ c.url.searchengines = {'DEFAULT': 'https://www.duckduckgo.com/lite/?q={}'}
 
 c.content.blocking.method = 'both'
 c.content.headers.referer = 'same-domain'
-c.content.cookies.accept = 'never'
+c.content.cookies.accept = 'all'
 
 # Put a domain in ./whitelist.txt to enable cookies on that website.
 # Example: *://*.google.com/*
-whitelist_path = os.path.join(config.configdir, 'whitelist.txt')
-if os.path.exists(whitelist_path):
-    with open(whitelist_path, 'r') as f:
-        for line in f:
-            pattern = line.strip()
-            if pattern and not pattern.startswith('#'):
-                config.set('content.cookies.accept', 'all', pattern)
+#whitelist_path = os.path.join(config.configdir, 'whitelist.txt')
+#if os.path.exists(whitelist_path):
+#    with open(whitelist_path, 'r') as f:
+#        for line in f:
+#            pattern = line.strip()
+#            if pattern and not pattern.startswith('#'):
+#                config.set('content.cookies.accept', 'all', pattern)
 
 config.source('qutemacs.py') # Keybinds
 config.source('gruvbox.py') # Colorscheme
